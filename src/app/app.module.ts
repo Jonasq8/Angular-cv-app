7a9ba708c28458034a0eccf23c6aaf1288cf9d53
Jonasq8 <1929jonas@gmail.com>
2024-05-31 11:30:07 +0200

    CSV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,38 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QualificationComponent } from './qualification/qualification.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { QualificationCardComponent } from './qualification/qualification-card/qualification-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { ExpericenceCardComponent } from './experience/expericence-card/expericence-card.component';
+import { ProfileIntroComponent } from './profile-intro/profile-intro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QualificationComponent,
+    ExperienceComponent,
+    QualificationCardComponent,
+    ExpericenceCardComponent,
+    ProfileIntroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
